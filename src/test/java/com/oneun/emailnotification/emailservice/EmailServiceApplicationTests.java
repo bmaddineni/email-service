2a1b085224901fcff98070555adcfec0b35ed3da
bharat <maddinenib@hotmail.com>
2022-10-06 13:24:54 -0400

    Initial commit

---
+++ src/test/java/com/oneun/emailnotification/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneun.emailnotification.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
